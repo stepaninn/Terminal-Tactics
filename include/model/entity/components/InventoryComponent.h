d778041acba32a83b9714e24e3c4b416731100e7
lisenenkov.ss <lisenenkov.ss@samos.dozen.mephi.ru>
2025-12-18 18:45:11 +0300

    updated IventoryComponent logic

--- include/model/entity/components/InventoryComponent.h
+++ include/model/entity/components/InventoryComponent.h
@@ -6,22 +6,22 @@
 #include "../entities/items/Item.h"
 #include <vector>
 #include <memory>
+#include <unordered_map>
 
 namespace game {
 
 class InventoryComponent : public IComponent {
 public:
-    virtual void add(std::unique_ptr<Item> item) = 0;
-    virtual std::unique_ptr<Item> remove(const Item* item) = 0;
-    virtual std::unique_ptr<Item> remove_at(size_t idx) = 0;
+    virtual void add(std::unique_ptr<Item> item, id_t id) = 0;
+    virtual std::unique_ptr<Item> remove_by_id(id_t id) = 0;
 
     [[nodiscard]] virtual size_t size() const noexcept = 0;
-    [[nodiscard]] virtual const Item* get_item(std::size_t idx) const = 0;
-    [[nodiscard]] virtual Item* get_item(size_t idx) = 0;
+    [[nodiscard]] virtual Item* get_item(id_t id) noexcept = 0;
 
-    [[nodiscard]] virtual int get_weight() const = 0;
-    [[nodiscard]] virtual int get_capacity() const = 0;
-    [[nodiscard]] virtual bool can_add(const Item& item) const = 0;
+    [[nodiscard]] virtual int get_weight() const noexcept = 0;
+    [[nodiscard]] virtual int get_capacity() const noexcept = 0;
+    [[nodiscard]] virtual int get_max_weight() const noexcept = 0;
+    [[nodiscard]] virtual bool can_add(const Item& item) const noexcept = 0;
 
     [[nodiscard]] virtual std::vector<const Item*> get_items() const = 0;
 
@@ -30,26 +30,26 @@ public:
 
 class DefaultInventoryComp : public InventoryComponent {
 public:
-    explicit DefaultInventoryComp(const int capacity = 0) : capacity_(capacity) {}
+    explicit DefaultInventoryComp(int capacity, int max_weight) : capacity_(capacity), max_weight_(max_weight) {}
 
-    void add(std::unique_ptr<Item> item) override;
-    std::unique_ptr<Item> remove(const Item* item) override;
-    std::unique_ptr<Item> remove_at(size_t idx) override;
+    void add(std::unique_ptr<Item> item, id_t id) override;
+    std::unique_ptr<Item> remove_by_id(id_t id) override;
 
     [[nodiscard]] size_t size() const noexcept override { return items_.size(); }
-    [[nodiscard]] Item* get_item(size_t idx) override { return items_.at(idx).get(); }
-    [[nodiscard]] const Item* get_item(size_t idx) const override { return items_.at(idx).get(); }
+    [[nodiscard]] Item* get_item(id_t id) noexcept override { return items_[id].get(); }
 
-    [[nodiscard]] int get_weight() const override { return weight_; }
-    [[nodiscard]] int get_capacity() const override { return capacity_; }
-    [[nodiscard]] bool can_add(const Item& item) const override { return item.get_weight() + weight_ <= capacity_; }
+    [[nodiscard]] int get_weight() const noexcept override { return weight_; }
+    [[nodiscard]] int get_capacity() const noexcept override { return capacity_; }
+    [[nodiscard]] int get_max_weight() const noexcept override { return max_weight_; }
+    [[nodiscard]] bool can_add(const Item& item) const noexcept override;
 
     [[nodiscard]] std::vector<const Item*> get_items() const override;
 
 protected:
     int capacity_ = 0;
     int weight_ = 0;
-    std::vector<std::unique_ptr<Item>> items_;
+    int max_weight_ = 0;
+    std::unordered_map<id_t, std::unique_ptr<Item>> items_;
 };
 
 } // namespace game
--- libs/model/entity/components/InventoryComponent.cpp
+++ libs/model/entity/components/InventoryComponent.cpp
@@ -8,40 +8,27 @@
 
 namespace game {
 
-void DefaultInventoryComp::add(std::unique_ptr<Item> item) {
-    if (!can_add(*item)) {
-        throw std::runtime_error("No enough weight");
-    }
+bool DefaultInventoryComp::can_add(const Item& item) const noexcept {
+    if (items_.size() == capacity_) return false;
+    return item.get_weight() + weight_ <= max_weight_;
+}
+
+void DefaultInventoryComp::add(std::unique_ptr<Item> item, id_t id) {
+    if (items_.size() == capacity_) throw std::runtime_error("Not enough space");
+    if (item->get_weight() + weight_ >= max_weight_) throw std::runtime_error("Item is too heavy");
 
     int w = item->get_weight();
-    items_.push_back(std::move(item));
+    items_[id] = std::move(item);
     weight_ += w;
 }
 
-std::unique_ptr<Item> DefaultInventoryComp::remove(const Item* ptr) {
-    auto it = std::ranges::find_if(
-        items_,
-        [ptr](const std::unique_ptr<Item>& item) {
-            return item.get() == ptr;
-        }
-    );
+std::unique_ptr<Item> DefaultInventoryComp::remove_by_id(id_t id) {
+    auto it = items_.find(id);
 
     if (it == items_.end()) return nullptr;
 
-    int w = (*it)->get_weight();
-    auto res = std::move(*it);
-    items_.erase(it);
-    weight_ -= w;
-    return res;
-}
-
-std::unique_ptr<Item> DefaultInventoryComp::remove_at(size_t idx) {
-    if (idx >= items_.size()) return nullptr;
-
-    auto it = items_.begin() + idx;
-
-    int w = (*it)->get_weight();
-    auto res = std::move(*it);
+    int w = (it->second)->get_weight();
+    auto res = std::move(it->second);
     items_.erase(it);
     weight_ -= w;
     return res;
@@ -51,7 +38,7 @@ std::vector<const Item*> DefaultInventoryComp::get_items() const {
     std::vector<const Item*> res;
     res.reserve(items_.size());
     std::ranges::for_each(items_, [&res](const auto& item) {
-        res.push_back(item.get());
+        res.push_back(item.second.get());
     });
     return res;
 }
