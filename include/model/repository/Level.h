ddda60c76e88f290a14e415b95ae69aaaf61b355
lisenenkov.ss <lisenenkov.ss@samos.dozen.mephi.ru>
2025-12-21 01:00:24 +0300

    updated level logic

--- include/model/repository/Level.h
+++ include/model/repository/Level.h
@@ -1,49 +1,55 @@
 #ifndef INC_3_LEVEL_H
 #define INC_3_LEVEL_H
 
-#include "types.hpp"
-#include "Cell.hpp"
-#include <map>
+#include "../../types.h"
+#include "../entity/entities/Entity.h"
+#include "cells/Cell.h"
+#include "Matrix.h"
+#include <unordered_map>
 #include <vector>
 #include <memory>
 
 namespace game {
 
-using FieldMatrix = Matrix<std::shared_ptr<Cell>>;
+using FieldMatrix = Matrix<std::unique_ptr<ICell>>;
 
 class Level {
 public:
-    Level() = default;
+    Level() = delete;
     explicit Level(id_t id, std::string name);
 
-    const FieldMatrix& get_field() const noexcept { return field_; }
-    FieldMatrix& get_field() noexcept { return field_; }
+    [[nodiscard]] const FieldMatrix& get_field() const noexcept { return field_; }
+    [[nodiscard]] FieldMatrix& get_field() noexcept { return field_; }
 
-    const std::map<id_t, std::shared_ptr<Entity>>& get_entities() const noexcept { return entities_; }
-    std::map<id_t, std::shared_ptr<Entity>>& get_entities() noexcept { return entities_; }
+    [[nodiscard]] Entity* get_entity(id_t id) { return entities_[id].get(); }
+    [[nodiscard]] std::vector<const Entity*> get_entities() noexcept;
 
-    std::weak_ptr<Cell> get_cell(const Position& pos) const noexcept;
+    [[nodiscard]] ICell* get_cell(Position pos) const noexcept;
+    std::unique_ptr<ICell> set_cell(Position pos, std::unique_ptr<ICell> cell);
 
-    bool set_cell(const Position& pos, std::shared_ptr<Cell> cell);
+    void spawn_entity(std::unique_ptr<Entity> e, Position pos);
 
-    bool spawn_entity(std::shared_ptr<Entity> e, const Position& pos);
+    std::unique_ptr<Entity> remove_entity(const Entity* e);
+    std::unique_ptr<Entity> remove_entity(id_t id);
 
-    const std::shared_ptr<Entity>& remove_entity(const std::shared_ptr<Entity>& e);
+    [[nodiscard]] std::vector<const Entity*> get_entities_radius(Position pos, int r) const;
 
-    std::vector<std::shared_ptr<Entity>> get_entities_radius(const Position& pos, int r) const;
+    void resize_field(size_t rows, size_t cols) { field_.resize(rows, cols); }
 
-    bool resize_field(std::size_t rows, std::size_t cols);
-
-    id_t get_id() const noexcept { return id_; }
+    [[nodiscard]] id_t get_id() const noexcept { return id_; }
     const std::string& get_name() const noexcept { return name_; }
-    void set_name(std::string new_name) { name_ = new_name; }
+    void set_name(const std::string& new_name) { name_ = new_name; }
 
 private:
-    id_t id_{0};
+    id_t id_;
     std::string name_;
 
     FieldMatrix field_;
-    std::map<id_t, std::shared_ptr<Entity>> entities_;
+
+    std::unordered_map<id_t, std::unique_ptr<Entity>> entities_;
+    std::unordered_map<id_t, Position> entity_positions_;
+
+    id_t next_entity_id_{1};
 };
 
 } // namespace game
