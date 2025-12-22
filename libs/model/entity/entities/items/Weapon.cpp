f20929011fd849dc57b1ff4aef651a16558bbb9d
lisenenkov.ss <lisenenkov.ss@samos.dozen.mephi.ru>
2025-12-22 17:45:39 +0300

    fixed Weapon impl

--- libs/model/entity/entities/items/Weapon.cpp
+++ libs/model/entity/entities/items/Weapon.cpp
@@ -2,13 +2,13 @@
 
 namespace game {
 
-int AmmoBag::add_ammo(int ammo) {
+int Weapon::add_ammo(int ammo) {
     int added = std::min(max_ammo_ - current_ammo_, ammo);
     current_ammo_ += added;
     return added;
 }
 
-int AmmoBag::reduce_ammo(int ammo) {
+int Weapon::reduce_ammo(int ammo) {
     int removed = std::min(current_ammo_, ammo);
     current_ammo_ -= removed;
     return removed;
