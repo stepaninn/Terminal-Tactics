86b5bae1ac4d686a4c99bf363c5fc3172a35f7d5
lisenenkov.ss <lisenenkov.ss@samos.dozen.mephi.ru>
2026-01-07 18:22:54 +0300

    small damage struct refactoring

--- include/types.h
+++ include/types.h
@@ -1,16 +1,16 @@
 #ifndef MYGAMEPROJECT_TYPES_H
 #define MYGAMEPROJECT_TYPES_H
 
-#include <stdexcept>
 #include <algorithm>
+#include <cstdint>
 
 namespace game {
 
-using id_t = size_t;
-using EntityId = size_t;
-using LevelId = size_t;
-using ItemId = size_t;
-using TeamId = size_t;
+using id_t = uint64_t;
+using EntityId = uint64_t;
+using LevelId = uint64_t;
+using ItemId = uint64_t;
+using TeamId = uint64_t;
 
 enum class CellType {
     FLOOR,
@@ -35,9 +35,8 @@ struct Damage {
     int min_dmg = 0;
     int max_dmg = 0;
 
-    constexpr Damage(int mn, int mx) : min_dmg(mn), max_dmg(mx) {
+    constexpr Damage(int mn, int mx) : min_dmg(std::max(0, mn)), max_dmg(std::max(0, mx)) {
         if (min_dmg > max_dmg) std::swap(min_dmg, max_dmg);
-        if (min_dmg < 0 || max_dmg < 0) throw std::invalid_argument("Damage must be non negative");
     }
 };
 
