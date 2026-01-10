2d10aa745fef2521129bb7cab75ea1958a5387c4
lisenenkov.ss <lisenenkov.ss@samos.dozen.mephi.ru>
2026-01-10 15:01:40 +0300

    realised controller

---
+++ include/controller/Controller.h
@@ -0,0 +1,86 @@
+#ifndef MYGAMEPROJECT_CONTROLLER_H
+#define MYGAMEPROJECT_CONTROLLER_H
+
+#include "model/service/services.h"
+#include "types.h"
+
+namespace game::controller {
+
+enum class InputAction {
+    NONE,
+    MOVE_UP,
+    MOVE_DOWN,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    CONFIRM,
+    CANCEL,
+    END_TURN,
+    TOGGLE_MODE,
+    QUIT
+};
+
+/// @brief Класс контроллер
+class Controller {
+public:
+    enum class Mode {
+        SELECT,
+        MOVE,
+        ATTACK
+    };
+
+    Controller(game::service::World& world,
+               game::service::TurnService& turn,
+               game::service::MovementService& move,
+               game::service::CombatService& combat,
+               game::service::ItemService& items);
+
+    /**
+     * @brief Метод обработки действия
+     * @param action Тип действия
+     * @return bool true, если действие успешно
+     */
+    [[nodiscard]] bool handle_action(InputAction action);
+
+    /**
+     * @brief Метод получения курсора на карте
+     * @return Позиция курсора на карте
+     */
+    [[nodiscard]] game::Position get_cursor() const noexcept { return cursor_; }
+    /**
+     * @brief Метод установки курсора на позицию
+     * @param pos Новая позиция курсора
+     */
+    void set_cursor(game::Position pos) noexcept { cursor_ = pos; }
+
+    /**
+     * @brief Метод получения ID выделенного существа
+     * @return ID выбранного существа
+     */
+    [[nodiscard]] game::EntityId selected_entity() const noexcept { return selected_; }
+    /**
+     * @brief Метод получения режима курсора
+     * @return Текущий режим курсора
+     */
+    [[nodiscard]] Mode get_mode() const noexcept { return mode_; }
+
+private:
+    void move_cursor(int dx, int dy);
+    bool select_entity_at_cursor();
+    bool move_selected_to_cursor();
+    bool attack_at_cursor();
+    void clear_selection() noexcept;
+
+    game::service::World& world_;
+    game::service::TurnService& turn_;
+    game::service::MovementService& move_;
+    game::service::CombatService& combat_;
+    game::service::ItemService& items_;
+
+    game::Position cursor_{0, 0};
+    game::EntityId selected_{game::service::TurnService::kNoEntity};
+    Mode mode_{Mode::SELECT};
+};
+
+}
+
+#endif // MYGAMEPROJECT_CONTROLLER_H
---
+++ libs/controller/Controller.cpp
@@ -0,0 +1,109 @@
+#include "controller/Controller.h"
+
+namespace game::controller {
+
+Controller::Controller(game::service::World& world,
+                       game::service::TurnService& turn,
+                       game::service::MovementService& move,
+                       game::service::CombatService& combat,
+                       game::service::ItemService& items)
+    : world_(world),
+      turn_(turn),
+      move_(move),
+      combat_(combat),
+      items_(items) {}
+
+bool Controller::handle_action(InputAction action) {
+    auto* level = world_.get_level();
+    if (!level) return action != InputAction::QUIT;
+
+    switch (action) {
+        case InputAction::MOVE_UP:
+            move_cursor(0, -1);
+            break;
+        case InputAction::MOVE_DOWN:
+            move_cursor(0, 1);
+            break;
+        case InputAction::MOVE_LEFT:
+            move_cursor(-1, 0);
+            break;
+        case InputAction::MOVE_RIGHT:
+            move_cursor(1, 0);
+            break;
+        case InputAction::CONFIRM:
+            if (mode_ == Mode::SELECT) {
+                if (select_entity_at_cursor()) {
+                    mode_ = Mode::MOVE;
+                }
+            } else if (mode_ == Mode::MOVE) {
+                move_selected_to_cursor();
+            } else if (mode_ == Mode::ATTACK) {
+                attack_at_cursor();
+            }
+            break;
+        case InputAction::CANCEL:
+            if (mode_ != Mode::SELECT) {
+                mode_ = Mode::SELECT;
+            } else {
+                clear_selection();
+            }
+            break;
+        case InputAction::TOGGLE_MODE:
+            if (selected_ == game::service::TurnService::kNoEntity) break;
+            mode_ = (mode_ == Mode::MOVE) ? Mode::ATTACK : Mode::MOVE;
+            break;
+        case InputAction::END_TURN:
+            clear_selection();
+            if (!turn_.end_entity_turn()) {
+                (void)turn_.next_team(*level);
+            }
+            break;
+        case InputAction::QUIT:
+            return false;
+        case InputAction::NONE:
+        default:
+            break;
+    }
+
+    return true;
+}
+
+void Controller::move_cursor(int dx, int dy) {
+    auto* level = world_.get_level();
+    if (!level) return;
+    game::Position next{cursor_.x + dx, cursor_.y + dy};
+    if (!level->in_bounds(next)) return;
+    cursor_ = next;
+}
+
+bool Controller::select_entity_at_cursor() {
+    auto* level = world_.get_level();
+    if (!level) return false;
+    auto* entity = level->get_entity_at(cursor_);
+    if (!entity) return false;
+    if (entity->get_team_id() != turn_.active_team()) return false;
+    if (!turn_.select_entity(*level, entity->get_id())) return false;
+    selected_ = entity->get_id();
+    return true;
+}
+
+bool Controller::move_selected_to_cursor() {
+    auto* level = world_.get_level();
+    if (!level) return false;
+    if (selected_ == game::service::TurnService::kNoEntity) return false;
+    return move_.move(*level, selected_, cursor_);
+}
+
+bool Controller::attack_at_cursor() {
+    auto* level = world_.get_level();
+    if (!level) return false;
+    if (selected_ == game::service::TurnService::kNoEntity) return false;
+    return combat_.try_shoot(*level, selected_, cursor_);
+}
+
+void Controller::clear_selection() noexcept {
+    selected_ = game::service::TurnService::kNoEntity;
+    mode_ = Mode::SELECT;
+}
+
+}
